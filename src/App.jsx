eb1dea27eb1e3ce77029116e82d903963939fc94
Samik Malhotra <samikmalhotra66@gmail.com>
2022-02-06 08:39:35 +0530

    added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Guidelines from './Components/Guidelines'
+import Landing from './Components/Landing/Landing'
 
 function App() {
 
   return (
     <div className="App">
+      <Landing/>
       <Guidelines/>
     </div> 
   )
