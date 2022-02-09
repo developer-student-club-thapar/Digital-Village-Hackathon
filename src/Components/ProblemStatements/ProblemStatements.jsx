150c01712e7f6b0e959ee42ca91b3bbbcde76e67
Sahaj <srivastavasahaj24@gmail.com>
2022-02-10 00:07:48 +0530

    styling done

--- src/Components/ProblemStatements/ProblemStatements.jsx
+++ src/Components/ProblemStatements/ProblemStatements.jsx
@@ -6,23 +6,58 @@ import styled from "@emotion/styled";
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   background-color: #072227;
+`;
+
+const StyledImg = styled.img`
+  position: relative;
+  top: 2rem;
+  left: 3.6rem;
+  height: 35px;
+  @media (max-width: 1200px) {
+    height: 30px;
+    top: 1.5rem;
+    left: 2.7rem;
+  }
+  @media (max-width: 950px) {
+    height: 25px;
+    top: 1.2rem;
+    left: 2.05rem;
+  }
 `;
 
 const HeadingNav = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  margin-bottom: 2.5rem;
 `;
 
 const HeadingTitle = styled.h1`
-  color: "#08BD04";
+  font-size: 4rem;
+  font-weight: bold;
+  letter-spacing: 0.1rem;
+  text-transform: uppercase;
+  color: #08bd04;
+  text-shadow: 4px 4px #000;
+  @media (max-width: 1200px) {
+    font-size: 3rem;
+  }
+  @media (max-width: 950px) {
+    font-size: 2rem;
+  }
 `;
 
 const StatementsWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  row-gap: 3rem;
+  column-gap: 4%;
+  margin: 2rem 4rem;
+  justify-content: space-around;
 `;
 
 function ProblemStatements() {
@@ -30,7 +65,7 @@ function ProblemStatements() {
     <>
       <Wrapper>
         <HeadingNav>
-          <img src={grid} alt="" />
+          <StyledImg src={grid} alt="" />
           <HeadingTitle>Problem Statement</HeadingTitle>
         </HeadingNav>
         <StatementsWrapper>
--- src/Components/ProblemStatements/StatementCard.jsx
+++ src/Components/ProblemStatements/StatementCard.jsx
@@ -1,28 +1,72 @@
 import React from "react";
+import grid from "./grid.svg";
+import svg from "./leaves.png";
 import styled from "@emotion/styled";
 
 const CardWrapper = styled.div`
   height: 30vh;
   width: 20vw;
   min-width: 200px;
-  min-height: 300px;
+  min-height: 350px;
+  max-width: 350px;
+  flex-grow: 1;
   border-radius: 1rem;
-  border: 1px solid #08bd04;
+  border: 3px solid #08bd04;
 `;
 
-const TitleWrapper = styled.div``;
+const CardContentWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 80%;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+const TitleWrapper = styled.div`
+  text-align: center;
+  margin: 0.5rem;
+`;
 
 const Title = styled.h5`
   color: white;
+  font-size: 1rem;
+  @media (max-width: 1200px) {
+    font-size: 0.9rem;
+  }
+  @media (max-width: 950px) {
+    font-size: 0.7rem;
+  }
+`;
+
+const StyledSvg = styled.img`
+  height: 100px;
+  width: 100px;
+`;
+
+const StyledImg = styled.img`
+  position: relative;
+  top: 2rem;
+  right: 1.3rem;
+  height: 35px;
 `;
 
 function StatementCard({ statement }) {
   return (
     <CardWrapper>
-      {/* <img src={grid} alt="" /> */}
-      <TitleWrapper>
-        <Title>{statement.title}</Title>
-      </TitleWrapper>
+      <StyledImg src={grid} alt="" />
+      <CardContentWrapper>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "centers",
+          }}
+        >
+          <StyledSvg src={svg} alt="" />
+        </div>
+        <TitleWrapper>
+          <Title>{statement.title}</Title>
+        </TitleWrapper>
+      </CardContentWrapper>
     </CardWrapper>
   );
 }
