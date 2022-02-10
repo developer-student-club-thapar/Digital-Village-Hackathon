f10ada9321faec36278fb0553fd342085e736a6f
Mad1ad <72143774+Sahaj-Srivastava24@users.noreply.github.com>
2022-02-10 20:32:40 +0530

    sponsors done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Guidelines from "./Components/Guidelines/Guidelines.jsx";
 import Landing from "./Components/Landing/Landing";
 import Speakers from "./Components/Speakers/Speakers";
 import ProblemStatements from "./Components/ProblemStatements/ProblemStatements";
-import Sponsor from "./Components/Sponsor/Sponsor";
+import Sponsors from "./Components/Sponsors/Sponsors";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
@@ -12,10 +12,10 @@ function App() {
       <Landing />
       <About />
       <Guidelines />
-      <Speakers />
-      <Sponsor />
       <ProblemStatements />
-      <Footer />
+      <Sponsors />
+      <Speakers />
+      {/* <Footer /> */}
     </div>
   );
 }
