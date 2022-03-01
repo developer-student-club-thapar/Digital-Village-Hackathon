e86d7c2db438ff4fe6d6e965f577f1d504987064
aniketbiswas21 <aniket.biswas75@gmail.com>
2022-03-01 22:07:27 +0530

    added more organisers
    Signed-off-by: aniketbiswas21 <aniket.biswas75@gmail.com>

--- src/Components/Organizers/organisers.js
+++ src/Components/Organizers/organisers.js
@@ -6,7 +6,9 @@ import namrata from "./pictures/namrata.jpeg";
 import prateek from "./pictures/Prateek.jpeg";
 import saurav from "./pictures/saurav.jpeg";
 import ujjwal from "./pictures/ujjwal.jpeg";
-import aniket from "./pictures/aniket.jpeg";
+import aniket from "./pictures/aniket.jpg";
+import yosis from "./pictures/yosis.webp";
+import mghosh from "./pictures/m_ghosh.jpeg";
 
 export default [
   {
@@ -35,30 +37,43 @@ export default [
   },
   {
     key: 5,
-    name: "Namrata Dimari",
-    des: "Backend@Wall | GSoC'21 | LFX Mentee'21 | KERNEL Fellow | xHabbit xLimeHealth| Former Research Intern @IIT Delhi,@IIIT Allahabad | MLH Fellow",
-    img: namrata,
-  },
-  {
-    key: 6,
     name: "Dr. Prateek Bhatia",
     des: "Incoming UX Research Intern @Meta",
     img: prateek,
   },
   {
+    key: 6,
+    name: "Prof. Yoshi Shacham",
+    des: "Incoming UX Research Intern @Meta",
+    img: yosis,
+  },
+  {
     key: 7,
+    name: "Dr. M Ghosh",
+    des: "Incoming UX Research Intern @Meta",
+    img: mghosh,
+  },
+  {
+    key: 8,
+    name: "Namrata Dimari",
+    des: "Backend@Wall | GSoC'21 | LFX Mentee'21 | KERNEL Fellow | xHabbit xLimeHealth| Former Research Intern @IIT Delhi,@IIIT Allahabad | MLH Fellow",
+    img: namrata,
+  },
+
+  {
+    key: 9,
     name: "Saurav Shrivastav",
     des: "Backend@Wall | GSoC'21 | LFX Mentee'21 | KERNEL Fellow | xHabbit xLimeHealth| Former Research Intern @IIT Delhi,@IIIT Allahabad | MLH Fellow",
     img: saurav,
   },
   {
-    key: 8,
+    key: 10,
     name: "Ujjwal Agarwal",
     des: "Incoming UX Research Intern @Meta",
     img: ujjwal,
   },
   {
-    key: 9,
+    key: 11,
     name: "Aniket Biswas",
     des: "Incoming UX Research Intern @Meta",
     img: aniket,
